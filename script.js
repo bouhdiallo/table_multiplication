0ec7cac2172aa71bccab0a22a3b94a75a1f86a8f
Bouhdiallo <bouhdiallo22@gmail.com>
2024-04-01 15:10:01 +0000

    ajout du script javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+let saisie= prompt("entrer une valeur");
+let depart = 0;
+0
+for (let i = 0; i < 10; i++){
+    depart = depart +1;
+    console.log(saisie+ " * " +depart+ " = " +saisie*depart);
+}
